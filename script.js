ea7d6ed7e2575de99e0445a0f467b4243bb94504
christiansaccani <christian.saccani99@gmail.com>
2024-02-13 21:27:40 +0100

    Exercise with Bonus Done!

--- script.js
+++ script.js
@@ -34,24 +34,8 @@ const members = [
 ];
 
 const table = document.querySelector("#table");
-const img0El = document.querySelector('.us_img_0');
-const name0El = document.querySelector('.us_name_0');
-const role0El = document.querySelector('.us_role_0');
-const img1El = document.querySelector('.us_img_1');
-const name1El = document.querySelector('.us_name_1');
-const role1El = document.querySelector('.us_role_1');
-const img2El = document.querySelector('.us_img_2');
-const name2El = document.querySelector('.us_name_2');
-const role2El = document.querySelector('.us_role_2');
-const img3El = document.querySelector('.us_img_3');
-const name3El = document.querySelector('.us_name_3');
-const role3El = document.querySelector('.us_role_3');
-const img4El = document.querySelector('.us_img_4');
-const name4El = document.querySelector('.us_name_4');
-const role4El = document.querySelector('.us_role_4');
-const img5El = document.querySelector('.us_img_5');
-const name5El = document.querySelector('.us_name_5');
-const role5El = document.querySelector('.us_role_5');
+const containerEl = document.querySelector(".container");
+
 
 // stampo su console le singole info di ogni membro
 
@@ -67,26 +51,31 @@ for (let i=0; i < members.length; i++) {
     }
 }
 
-name0El.innerHTML = members[0].name;
-role0El.innerHTML = members[0].role;
-img0El.src = members[0].pic;
+// -------------------------------------------------------------  //
 
-name1El.innerHTML = members[1].name;
-role1El.innerHTML = members[1].role;
-img1El.src = members[1].pic;
+for (let i=0; i < members.length; i++) {
+    
+    const newElement = document.createElement("div");
+    newElement.className = "card";
+    newElement.style.width = "18rem";
+    containerEl.append(newElement);
 
-name2El.innerHTML = members[2].name;
-role2El.innerHTML = members[2].role;
-img2El.src = members[2].pic;
+    let memberPic = members[i].pic;
+    let newPicElement = document.createElement('img');
+    newPicElement.className = "card-img-top";
+    newPicElement.src = memberPic;
+    newElement.append(newPicElement);
 
-name3El.innerHTML = members[3].name;
-role3El.innerHTML = members[3].role;
-img3El.src = members[3].pic;
+    let memberName = members[i].name;
+    let newNameElement = document.createElement('h5');
+    newNameElement.className = "card-title";
+    newNameElement.innerText = memberName;
+    newElement.append(newNameElement);
 
-name4El.innerHTML = members[4].name;
-role4El.innerHTML = members[4].role;
-img4El.src = members[4].pic;
+    let memberRole = members[i].role;
+    let newRoleElement = document.createElement('p');
+    newRoleElement.className = "card-text";
+    newRoleElement.innerText = memberRole;
+    newElement.append(newRoleElement);
 
-name5El.innerHTML = members[5].name;
-role5El.innerHTML = members[5].role;
-img5El.src = members[5].pic;
+}
